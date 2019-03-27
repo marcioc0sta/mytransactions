bee44c3e78c7de56e719227a1798ccc005d0c0db
marcioc0st <marcio@vizir.com.br>
2019-03-27 19:26:16 -0300

    feature: Add orderby month and year actions

--- src/actions/transactions.js
+++ src/actions/transactions.js
@@ -5,6 +5,8 @@ export const RECEIVE_ALL_TRANSACTIONS = 'RECEIVE_ALL_TRANSACTIONS'
 export const SAVE_TRANSACTION = 'SAVE_TRANSACTION'
 export const ORDER_TRANSACTIONS_BY_DATE = 'ORDER_TRANSACTIONS_BY_DATE'
 export const CALCULATE_TOTAL_OF_TRANSACTIONS = 'CALCULATE_TOTAL_OF_TRANSACTIONS'
+export const ORDERED_BY_MONTH = 'ORDERED_BY_MONTH'
+export const ORDERED_BY_YEAR = 'ORDERED_BY_YEAR'
 
 const receiveTransactions = transactions => {
   return {
@@ -34,6 +36,20 @@ const calculateTotalOfTransactions = transactions => {
   }
 }
 
+const orderTransactionsByMonth = transactions => {
+  return {
+    type: ORDERED_BY_MONTH,
+    transactions,
+  }
+}
+
+const orderTransactionsByYear = transactions => {
+  return {
+    type: ORDERED_BY_YEAR,
+    transactions,
+  }
+}
+
 export const handleReceiveTransactions = () => {
   const transactionList = getTransactionList()
   return dispatch => {
@@ -41,6 +57,8 @@ export const handleReceiveTransactions = () => {
     if (transactionList !== null) {
       dispatch(orderPostsByDate(transactionList))
       dispatch(calculateTotalOfTransactions(transactionList))
+      dispatch(orderTransactionsByMonth(transactionList))
+      dispatch(orderTransactionsByYear(transactionList))
     }
   }
 }
--- src/reducers/transactions.js
+++ src/reducers/transactions.js
@@ -1,15 +1,19 @@
 import { currencyStringToNumber } from '../helpers/currencyStringToNumber'
+import moment from 'moment'
 
 import {
   RECEIVE_ALL_TRANSACTIONS,
   SAVE_TRANSACTION,
   ORDER_TRANSACTIONS_BY_DATE,
   CALCULATE_TOTAL_OF_TRANSACTIONS,
+  ORDERED_BY_MONTH,
+  ORDERED_BY_YEAR,
 } from '../actions/transactions'
 
 const initialState = {
   list: [],
   total: 0,
+  orderedByMonth: {},
 }
 
 export default function transactions(state = initialState, action) {
@@ -28,7 +32,7 @@ export default function transactions(state = initialState, action) {
         list: [...orderedByTime],
       }
     case CALCULATE_TOTAL_OF_TRANSACTIONS:
-      const values = state.list.map(({value}) => currencyStringToNumber(value))
+      const values = state.list.map(({ value }) => currencyStringToNumber(value))
       return {
         ...state,
         total: values.reduce((initialVal, nextItem) => initialVal + nextItem),
@@ -39,6 +43,50 @@ export default function transactions(state = initialState, action) {
       return {
         ...state,
         list: updatedList
+      }
+    case ORDERED_BY_MONTH:
+      const transacitonsByMonth = state.list.reduce((transacitonsGroupByMonth, currentTransaction) => {
+        const transacitonMonth = moment(currentTransaction.timestamp).month()
+        const currentMonthTransactions = transacitonsGroupByMonth[transacitonMonth]
+        return currentMonthTransactions
+          ? {
+            ...transacitonsGroupByMonth,
+            [transacitonMonth]: [...currentMonthTransactions, currentTransaction],
+          }
+          : {
+            ...transacitonsGroupByMonth,
+            [transacitonMonth]: [currentTransaction]
+          }
+
+      }, {})
+
+      return {
+        ...state,
+        orderedByMonth: transacitonsByMonth
+      }
+    case ORDERED_BY_YEAR:
+      const year = new Date().getFullYear()
+      const initialObj = {
+        [year]: []
+      }
+      const transacitonsByYear = state.list.reduce((transacitonsGroupByYear, currentTransaction) => {
+        const transacitonYear = moment(currentTransaction.timestamp).year()
+        const currentYearTransactions = transacitonsGroupByYear[transacitonYear]
+        return currentYearTransactions
+          ? {
+            ...transacitonsGroupByYear,
+            [transacitonYear]: [...currentYearTransactions, currentTransaction],
+          }
+          : {
+            ...transacitonsGroupByYear,
+            [transacitonYear]: [currentTransaction]
+          }
+
+      }, initialObj)
+
+      return {
+        ...state,
+        orderedByYear: transacitonsByYear
       }
     default:
       return state
